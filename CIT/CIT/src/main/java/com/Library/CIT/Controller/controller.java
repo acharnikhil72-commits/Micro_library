99f2b7fc81bcdc906462e492cc12c39a0fcf6762
nikhil s achar <acharnikhil72@gmail.com>
2026-09-09 22:52:43 +0530

    small changes

--- CIT/CIT/src/main/java/com/Library/CIT/Controller/controller.java
+++ CIT/CIT/src/main/java/com/Library/CIT/Controller/controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import com.Library.CIT.Repo.model;
 import com.Library.CIT.Services.Services;
 
 @RestController
+@RequestMapping("/user")
 public class controller {
 
     @Autowired
