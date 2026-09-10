6ba43d097abf04aa292104fd265bf120bef161c0
nikhil s achar <acharnikhil72@gmail.com>
2026-09-10 13:34:54 +0530

    added DiscoveryClient , because RestTemplete is old version

--- CIT/CIT/src/main/java/com/Library/CIT/Services/Services.java
+++ CIT/CIT/src/main/java/com/Library/CIT/Services/Services.java
@@ -3,6 +3,7 @@ package com.Library.CIT.Services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 
 import org.springframework.web.client.RestTemplate;
@@ -12,10 +13,14 @@ import com.Library.CIT.Repo.repo;
 @Service
 public class Services {
 
-    private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
 
-    public Services(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Services(DiscoveryClient discoveryClient) {
+
+        this.discoveryClient = discoveryClient;
     }
 
     @Autowired
@@ -30,6 +35,13 @@ public class Services {
     }
 
     public List<model> Book_user_owned(Long id) {
-        return restTemplate.getForObject("http://localhost:8082/inventery/userid/{id}/getAll", List.class, id);
+        return discoveryClient.getInstances("LIB_BOOKS").stream()
+                .flatMap(instance -> {
+                    String url = instance.getUri() + "/inventory/userid/" + id + "/getAll";
+                    List result = restTemplate.getForObject(url, List.class);
+                    return result.stream();
+                })
+                .toList();
     }
+
 }
